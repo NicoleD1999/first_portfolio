a3515557828e0f06f967c714a69df9b4c9262a7d
NicoleD1999 <nmd1018@yahoo.com>
2022-04-08 18:04:07 -0500

    deleted commented out html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,4 @@ $('#lightDark').click(function() {
     $(document.body).toggleClass("darkMode")
     $("#darkNav").toggleClass("bg-dark")   
     $("#contactMe").toggleClass(".contactMeDark") 
-    
      });
